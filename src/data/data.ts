a1640578e13711c8abedac1f9983b7dc7a98829c
Diego <d.becerraz@uniandes.edu.co>
2024-09-24 22:42:17 -0500

    Feat: pagina de home y estilo de colores

---
+++ src/data/data.ts
@@ -0,0 +1,36 @@
+export var multi = [
+    {
+      "name": "Pomodoros completados",
+      "series": [
+        {
+          "name": "01-10-2024",
+          "value": 3
+        },
+        {
+          "name": "02-10-2024",
+          "value": 6
+        },
+        {
+          "name": "03-10-2024",
+          "value": 2
+        },
+        {
+          "name": "04-10-2024",
+          "value": 4
+        },
+        {
+          "name": "05-10-2024",
+          "value": 3
+        },
+        {
+          "name": "06-10-2024",
+          "value": 5
+        },
+        {
+          "name": "07-10-2024",
+          "value": 1
+        }
+      ]
+    },
+  ];
+  
--- src/pages/home/home.component.ts
+++ src/pages/home/home.component.ts
@@ -1,12 +1,55 @@
 import { Component } from '@angular/core';
+import { Color, NgxChartsModule, ScaleType } from '@swimlane/ngx-charts';
+import { multi } from '../../data/data';
+import { MatIconModule } from '@angular/material/icon';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    NgxChartsModule,
+    MatIconModule,
+    MatGridListModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+  ],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
+  multi: any[];
+  view: [number, number] = [700, 300];
 
+  // options
+  legend: boolean = true;
+  showLabels: boolean = true;
+  animations: boolean = true;
+  xAxis: boolean = true;
+  yAxis: boolean = true;
+  showYAxisLabel: boolean = true;
+  showXAxisLabel: boolean = true;
+  xAxisLabel: string = 'Dia';
+  yAxisLabel: string = 'Horas';
+  timeline: boolean = true;
+
+  colorScheme: Color = {
+    // Especifica el tipo de color correctamente
+    name: 'custom',
+    selectable: true,
+    group: ScaleType.Ordinal,
+    domain: ['#E07A5F'],
+  };
+
+  constructor() {
+    Object.assign(this, { multi });
+  }
+
+  onSelect(event: any) {
+    console.log(event);
+  }
 }
