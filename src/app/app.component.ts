ffcda08a74594b80a7f4a4d895a9039e99c5c196
Diego <d.becerraz@uniandes.edu.co>
2024-09-26 18:50:18 -0500

    Feat: icono de tomate y modificacion de estilo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from '../components/header/header.component';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet,HeaderComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  providers: [MatIconRegistry,]
 })
 export class AppComponent {
   title = 'pomochan';
+
+  constructor(private matIconRegistry: MatIconRegistry, private domSanitizer: DomSanitizer) {
+    this.matIconRegistry.addSvgIcon(
+      'tomato',
+      this.domSanitizer.bypassSecurityTrustResourceUrl('icons/tomato.svg')
+    );
+    
+  }
 }
